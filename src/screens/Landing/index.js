f02b633e799d8ae2337fce1ac101970cc7e74b07
hidanscript <hidanshira2@gmail.com>
2020-05-17 23:38:38 -0300

    First commit

---
+++ src/screens/Landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Landing() {
+    return (
+        <div class="hero">
+            <svg 
+                className="waves"
+                viewBox="0 0 500 150" 
+                preserveAspectRatio="none" >
+                <path d="M0.00,49.98 C149.99,150.00 359.76,-2.45 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" ></path>
+            </svg>
+        </div>
+    )
+}
